da6a2646f2a04b35133fc68d5ccbc90f32b95db5
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2021-02-07 19:35:24 +0100

    finish

--- src/components/account.js
+++ src/components/account.js
@@ -18,29 +18,41 @@ function Account() {
 
     function handleSubmit(e) {
         e.preventDefault();
+        // if (e.target[0].value.trim() && e.target[1].value.trim() && e.target[2].value.trim()) {
+        //     if (name.indexOf(' ') == -1) {
+        //         console.log('there is no  white space')
+        //     } else   console.log('you can not have a white space')
+
+
+
+
+
         let name = e.target[0].value;
         let email = e.target[1].value;
         let password = e.target[2].value;
-        console.log(e)
+        // console.log(e)
+        if (name.trim() && name.indexOf(' ') == -1 && email.trim() && email.indexOf(' ') == -1 && password.trim() && password.indexOf(' ') == -1) {
 
-        if (buttonText === 'Sign me up!') {
-            setInputClass('input-custom')
-            dispatch(setUserData(name, email, password))
-            setButtonText('Ändra')
-            setPageTitle('Ditt konto')
-            history.push('/account')
+            if (buttonText === 'Sign me up!') {
+                setInputClass('input-custom')
+                dispatch(setUserData(name, email, password))
+                setButtonText('Ändra')
+                setPageTitle('Ditt konto')
+                history.push('/account')
+            }
 
-        }
-        if (buttonText === 'Ändra') {
-            setInputClass('input-normal')
-            setButtonText('Spara')
-        }
+            if (buttonText === 'Ändra') {
+                setInputClass('input-normal')
+                setButtonText('Spara')
+            }
 
-        if (buttonText === 'Spara') {
-            setInputClass('input-custom')
-            dispatch(setUserData(name, email, password))
-            setButtonText('Ändra')
-        }
+            if (buttonText === 'Spara') {
+                setInputClass('input-custom')
+                dispatch(setUserData(name, email, password))
+                setButtonText('Ändra')
+            }
+
+        } else alert('Du får inte skriva mellanslag som inmatning')
     }
 
 
@@ -49,17 +61,17 @@ function Account() {
         <div className='account-container'>
             <h3> {pageTitle}</h3>
             <form className='sign-in-form' onSubmit={handleSubmit} >
-                        <label>NAME</label>
-                        <input type="text" className={inputClass}  id='name' required />
-                
-                        <label >EMAIL</label>
-                        <input   type="text" className={inputClass} id='email' required />
-                  
-                        <label >PASSWORD</label>
-                        <input type="password" className={inputClass} id='password' required />
-             
-                        <input type='submit' onSubmit={handleSubmit} value={buttonText} />
-            
+                <label>NAME</label>
+                <input type="text" className={inputClass} id='name' required />
+
+                <label >EMAIL</label>
+                <input type="text" className={inputClass} id='email' required />
+
+                <label >PASSWORD</label>
+                <input type="password" className={inputClass} id='password' required />
+
+                <input type='submit' onSubmit={handleSubmit} value={buttonText} />
+
 
             </form>
 
